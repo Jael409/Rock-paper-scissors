4031d4d59440a0a9cf310a8de06bb380fa7f861a
Jael409 <179689911+Jael409@users.noreply.github.com>
2024-11-22 12:04:26 +0100

    Add files via upload
    Add new file css, html and js modified

--- javascript.js
+++ javascript.js
@@ -1,111 +1,87 @@
-
-function getHumanChoice () {
-    eleccion = prompt("Ingresa tu elección");
-    if (eleccion.toLowerCase() === "rock") {
-        return "rock";
-    }
-    else if (eleccion.toLowerCase() === "paper") {
-        return "paper";
-    }
-    else if (eleccion.toLowerCase() === "scissors") {
-        return "scissors";
-    }
-
-}
+const rock = document.querySelector(".rockBtn");
+rock.addEventListener("click", playRound);
 
 
+const paper = document.querySelector(".paperBtn");
+paper.addEventListener("click", playRound);
+
+const scissors = document.querySelector(".scissorsBtn");
+scissors.addEventListener("click", playRound);
+
+       
 function getComputerChoice () {
     result = Math.floor(Math.random() * 3) +1;
     if (result == 1) {
-        console.log("rock");
+        alert("Rock");
     }
     else if (result == 2) {
-        console.log("paper");
+        alert("Paper");
     }
-    else {
-        console.log("scissors");
+    else if (result == 3) {
+        alert("Scissors");
     }
     return result;
 }
 
-console.log();
-
-
 let humanScore = 0;
 let computerScore = 0; 
 
-function playRound() {
 
-    let eleccion = getHumanChoice();
+function playRound(e) {
     let result = getComputerChoice(); 
-
-    if (
-        (eleccion === "rock" && result === 1) ||
-        (eleccion === "paper" && result === 2) ||
-        (eleccion === "scissors" && result === 3)
-    ) {
-        console.log("Draw!");
-    }
-    else if (eleccion === "rock" && result === 2) {
-        
-        console.log("You lose! the paper beats the stone");
-        computerScore++;  
-    }
-    else if (eleccion === "paper" && result === 3) {
-        
-        console.log("You lose! the scissors beat the paper")
-        computerScore++;
-    }
-    else if (eleccion === "scissors" && result === 1) {
-        
-        console.log ("You lose! the stone beats scissors")
-        computerScore++;
-    }
-    else if (eleccion === "rock" && result === 3) {
-        
-        console.log ("You win! stone beats scissors")
-        humanScore++;
-    }
-    else if (eleccion === "paper" && result === 1) {
-        
-        console.log ("You win! Paper beats stone")
-        humanScore++;
-    }
-    else if (eleccion === "scissors" && result === 2) {
-
-        console.log ("You win! Scissors beat paper")
-        humanScore++;
-
-    }else {
-        console.log("Entry no valid");
-    }
-}
-        
-
-function playGame () {
-    for (let i = 0; i < 5; i++) {
-        playRound();       
-    }
-}
-
-playGame();
-
-console.log("Your score is " + humanScore + " Machine's score is " + computerScore);
-
-
-function score() {
-    if (humanScore === computerScore) {
-        console.log ("Draw! Your score: " + humanScore + " Opponent score: " + computerScore);
-    }
+    let eleccion = e.currentTarget.id;
     
-    if (humanScore > computerScore) {
-        console.log("Win! Your score: " + humanScore + " Opponent score: " + computerScore);
+
+    if ((eleccion === "rBtn" && result === 3) || 
+        (eleccion === "pBtn" && result === 1) ||
+        (eleccion === "sBtn" && result === 2)) {
+        userWin();
+    } 
+    else if ((eleccion === "rBtn" && result === 2) || 
+            (eleccion === "pBtn" && result === 3) ||
+            (eleccion === "sBtn" && result === 1)) {
+        pcWin();
     }
+    else {
+        draw();
+    }
+
+    if (humanScore === 5 || computerScore === 5) {
+
+        if (humanScore === 5) {
+            notice.textContent = "Whhhhooooaaaa! You Win!";
+        }
     
-    if (humanScore < computerScore) {
-        console.log("You lose! Your score: " + humanScore + " Opponent score " + computerScore);
+        if (computerScore === 5) {
+            notice.textContent = "Sorry about that...You lose!"
+        }
+    
     }
 }
+       
+let divUsuario = document.querySelector("#userPoints");
 
-score()
+function userWin () {
+    humanScore++;
+    divUsuario.textContent = humanScore;
+    divscoreUser.textContent = "You Win 1 point!";
+}
+
+let divPc = document.querySelector("#pcPoints");
+let divscoreUser = document.querySelector("#contUser");
+
+let divcontPc = document.querySelector("#contPc");
+
+function pcWin () {
+    computerScore++;
+    divPc.textContent = computerScore;
+    divcontPc.textContent = "Pc win 1 point";
+}
+
+function draw () {
+    alert("DRAW!");
+}
+
+let notice = document.querySelector("#textNotice");
+
 
